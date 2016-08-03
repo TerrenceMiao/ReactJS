5aa9ba72c4f293705eb8a24163d799fda765bcae
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-08-03 12:34:00 +1000

    Add Ajax call to ElasticSearch service by using Fetch API

--- auto-suggest/js/app.js
+++ auto-suggest/js/app.js
@@ -4,6 +4,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Autosuggest = require('react-autosuggest');
 
+//
+// React example
+//
+
 var ExampleApplication = React.createClass({
     render: function () {
         var elapsed = Math.round(this.props.elapsed / 100);
@@ -20,11 +24,11 @@ setInterval(function () {
     ReactDOM.render(<ExampleApplication elapsed={new Date().getTime() - start}/>, document.getElementById('container'));
 }, 50);
 
+//
+// App
+//
 
-/* ---------- */
-/*    Data    */
-/* ---------- */
-
+/* Data */
 const languages = [
     {
         name: 'C',
@@ -96,10 +100,43 @@ function getMatchingLanguages(value) {
     return languages.filter(language => regex.test(language.name));
 }
 
-/* ----------- */
-/*    Utils    */
-/* ----------- */
+function getMatchingPostalAddresses(clazz) {
 
+    var headers = new Headers();
+    headers.append('Content-Type', 'x-www-form-urlencoded');
+    headers.append('Accept', 'application/json, text/plain, */*');
+
+    var init = {
+        method: 'POST',
+        headers: headers,
+        mode: 'cors',
+        body: JSON.stringify({"query":{"query_string":{"fields":["house_nbr_1","street_name","street_type","locality_name","state","postcode"],"query":"00018 Balfour Cl Point Cook VIC 3030"}}}),
+        redirect: 'follow',
+        cache: 'default'
+    };
+
+    var request = new Request('http://localhost:9200/postaladdress/_search', init);
+
+    fetch(request)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(JSON.stringify(data));
+
+            const suggestions = data.hits.hits;
+
+            clazz.setState({
+                isLoading: false,
+                suggestions
+            });
+        })
+        .catch(function (error) {
+            console.log('Request failed with error: ', error);
+        });
+}
+
+/* Utils */
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -109,21 +146,21 @@ function randomDelay() {
     return 300 + Math.random() * 1000;
 }
 
-/* --------------- */
-/*    Component    */
-/* --------------- */
-
+/* Component */
 function getSuggestionValue(suggestion) {
-    return suggestion.name;
+    // return suggestion.name;
+    return suggestion._source.delivy_point_id;
 }
 
 function renderSuggestion(suggestion) {
     return (
-        <span>{suggestion.name}</span>
+        // <span>{suggestion.name}</span>
+        <span>{suggestion._source.delivy_point_id}</span>
     );
 }
 
 class App extends React.Component {
+
     constructor() {
         super();
 
@@ -142,7 +179,8 @@ class App extends React.Component {
             isLoading: true
         });
 
-        // Fake an AJAX call
+        // Make an AJAX call
+/*
         setTimeout(() => {
             const suggestions = getMatchingLanguages(value);
 
@@ -157,6 +195,9 @@ class App extends React.Component {
                 });
             }
         }, randomDelay());
+*/
+
+        getMatchingPostalAddresses(this);
     }
 
     onChange(event, { newValue }) {
