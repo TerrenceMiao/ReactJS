e1d3efb9aff44f5a7f183c2bdad8d87700ef6a74
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-08-20 13:26:15 +1000

    Refactor webpack configuration file

--- auto-suggest/webpack.config.js
+++ auto-suggest/webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
         './index'
     ],
     output: {
-        path: path.join(__dirname, '.'),
-        filename: 'index.js',
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
         publicPath: '/'
     },
     plugins: [
