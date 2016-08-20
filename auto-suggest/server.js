fdccaaeb9d1564af22fa08381facc1fbcadf499b
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-08-21 03:09:34 +1000

    Enable webpack

--- auto-suggest/server.js
+++ auto-suggest/server.js
@@ -1,18 +1,27 @@
-var express = require('express');
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-var app = express();
-var port = 3000;
+var express = require('express')
 
-app.use(express.static(__dirname + '/'));
+var app = express()
+var port = 3000
+
+app.use(express.static(__dirname + '/'))
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
-});
+})
 
 app.listen(port, function(error) {
     if (error) {
-        console.error(error);
+        console.error(error)
     } else {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s in your browser.", port, port);
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s in your browser.", port, port)
     }
 })
