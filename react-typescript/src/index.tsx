54908bf76c2e4621fc15be53cd961b20669305d7
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2018-08-03 13:50:51 +1000

    Add comments

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -12,8 +12,10 @@ import Hello from './containers/Hello';
 
 import './index.css';
 
-const store = createStore<IStoreState, EnthusiasmAction, null, null>(enthusiasmReducer, 
-    { enthusiasmLevel: 1, languageName: 'TypeScript' }, 
+const store = createStore<IStoreState, EnthusiasmAction, null, null>(enthusiasmReducer,
+    // Preloaded state
+    { enthusiasmLevel: 1, languageName: 'TypeScript' },
+    // Added for Chrome's Redux Devtools Extenstion support
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
