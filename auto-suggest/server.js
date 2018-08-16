da3cfcda61fbb968bbc8a579364639831e551f7e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2018-08-16 10:16:47 +1000

    Comment out opernUrl due to error raised run in Docker

--- auto-suggest/server.js
+++ auto-suggest/server.js
@@ -20,5 +20,27 @@ new WebpackDevServer(webpack(config), {
 
     console.log('==> 🌎 Listening on port %s. Open up %s in your browser.', port, url);
 
-    openUrl.open(url);
+    // Error thrown when 
+    // events.js:183
+    //   throw er; // Unhandled 'error' event
+    //   ^
+    //
+    // Error: spawn xdg-open ENOENT
+    //     at _errnoException (util.js:992:11)
+    //     at Process.ChildProcess._handle.onexit (internal/child_process.js:190:19)
+    //     at onErrorNT (internal/child_process.js:372:16)
+    //     at _combinedTickCallback (internal/process/next_tick.js:138:11)
+    //     at process._tickCallback (internal/process/next_tick.js:180:9)
+    //     at Function.Module.runMain (module.js:695:11)
+    //     at startup (bootstrap_node.js:191:16)
+    //     at bootstrap_node.js:612:3
+    // npm ERR! code ELIFECYCLE
+    // npm ERR! errno 1
+    // npm ERR! Postal-Address-auto-suggest@0.0.1 start: `npm run watch & node server`
+    // npm ERR! Exit status 1
+    // npm ERR!
+    // npm ERR! Failed at the Postal-Address-auto-suggest@0.0.1 start script.
+    // npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+
+    // openUrl.open(url);
 });
